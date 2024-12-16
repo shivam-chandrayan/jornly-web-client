212b89d5fcb091954d1d5b0f914e01fb5285b63b
Shivam Kumar <shivam.chandrayan@gmail.com>
2024-12-16 22:40:04 +0530

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
+import Editor from "./Editor";
 
 function App() {
   return (
-    <>
-      <Dashboard />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/editor" element={<Editor />} />
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
